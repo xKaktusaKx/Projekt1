284391030202336615e8dd1c1066d4c4e52d1df6
angry_vojta <nevim>
2025-03-02 20:51:00 +0100

    Update settings.py for ALLOWED_HOSTS

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d*=5^zy+okot()eb476-p-%ghei7g13eli$cwa^dtb*uppr5o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [angryvojta.pythonanywhere.com]
 
 
 # Application definition
