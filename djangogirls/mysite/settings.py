e1679b140505ff3fdc414ea2a6504414d579705b
angry_vojta <nevim>
2025-03-03 19:31:40 +0100

    CSS stránka vypadá dobře snad.

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d*=5^zy+okot()eb476-p-%ghei7g13eli$cwa^dtb*uppr5o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []  # For production, specify the allowed hosts like: ['example.com']
+ALLOWED_HOSTS = ['*']  # For production, specify the allowed hosts like: ['example.com']
 
 # Application definition
 
